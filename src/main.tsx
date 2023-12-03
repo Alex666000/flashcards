24101f779d3d238ae3ada1c3aecde730bf61153e
Alex666000 <bogdanov777000@mail.ru>
2023-12-03 13:25:20 +0300

    add fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 import { App } from './App'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
