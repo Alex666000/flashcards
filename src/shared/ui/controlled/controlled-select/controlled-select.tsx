752394a3d45f06ad09146c71e076565aed0f8e2b
Alex666000 <bogdanov777000@mail.ru>
2024-02-07 16:50:46 +0300

    add me()

---
+++ src/shared/ui/controlled/controlled-select/controlled-select.tsx
@@ -0,0 +1,21 @@
+import { FieldValues, UseControllerProps, useController } from 'react-hook-form'
+
+import { Select, SelectProps } from '@/components'
+
+type ControlledSelectProps<T extends FieldValues> = UseControllerProps<T> &
+    Omit<SelectProps, 'onValueChange' | 'value'>
+
+export const ControlledSelect = <T extends FieldValues>({
+    control,
+    name,
+    ...restProps
+}: ControlledSelectProps<T>): JSX.Element => {
+    const {
+        field: { onChange, value },
+    } = useController({
+        control,
+        name,
+    })
+
+    return <Select {...restProps} onValueChange={onChange} value={value} />
+}
