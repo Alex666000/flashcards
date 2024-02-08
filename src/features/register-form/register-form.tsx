6e958d5ff54fad43aadffbb3bc1f28ce8ce3c91a
Alex666000 <bogdanov777000@mail.ru>
2024-02-08 21:37:29 +0300

    fix SingUp_page

--- src/features/register-form/register-form.tsx
+++ src/features/register-form/register-form.tsx
@@ -1,56 +1,52 @@
 import { FC } from 'react'
-
 import { Link } from 'react-router-dom'
 
-import s from './register-form.module.scss'
-import { RegisterFormType, useRegisterForm } from './use-register-form.ts'
+import { RegisterFormType, useRegisterForm } from '@/features/register-form/use-register-form'
+import { ROUTES } from '@/shared/common/constants'
+import { Button } from '@/shared/ui/button'
+import { Card } from '@/shared/ui/card'
+import { ControlledTextField } from '@/shared/ui/controlled'
+import { Typography } from '@/shared/ui/typography'
 
-import { PATH } from '@/common'
-import { Button } from '@/components/ui/button'
-import { Card } from '@/components/ui/card'
-import { ControlledInput } from '@/components/ui/controlled'
-import { Typography } from '@/components/ui/typography'
-import { useTranslate } from '@/i18n.ts'
+import s from './register-form.module.scss'
 
 type RegisterFormPropsType = {
   onSubmitHandler: (data: RegisterFormType) => void
 }
 
 export const RegisterForm: FC<RegisterFormPropsType> = ({ onSubmitHandler }) => {
-  const t = useTranslate()
-
-  const { handleSubmit, control } = useRegisterForm()
-  const onSubmit = handleSubmit(data => {
+  const { control, handleSubmit } = useRegisterForm()
+  const onSubmit = handleSubmit((data) => {
     onSubmitHandler(data)
   })
 
   return (
     <Card className={s.card}>
-      <Typography className={s.title} as="h1" variant="large">
-        {t('Sign Up')}
+      <Typography as={'h1'} className={s.title} variant={'large'}>
+        Sign Up
       </Typography>
-      <form onSubmit={onSubmit} className={s.form}>
-        <ControlledInput label={t('Email')} name={'email'} control={control} />
-        <ControlledInput
-          label={t('Password')}
+      <form className={s.form} onSubmit={onSubmit}>
+        <ControlledTextField control={control} label={'Email'} name={'email'} />
+        <ControlledTextField
+          control={control}
+          label={'Password'}
           name={'password'}
-          type="password"
-          control={control}
+          type={'password'}
         />
-        <ControlledInput
-          label={t('Confirm Password')}
+        <ControlledTextField
+          control={control}
+          label={'Confirm Password'}
           name={'confirm'}
-          type="password"
-          control={control}
+          type={'password'}
         />
-        <Button fullWidth className={s.button} type="submit">
-          <Typography variant="subtitle2">{t('Sign Up')}</Typography>
+        <Button className={s.button} fullWidth type={'submit'}>
+          <Typography variant={'subtitle2'}>Sign Up</Typography>
         </Button>
-        <Typography className={s.subtitle} variant="body2">
-          {t('Already have an account?')}
+        <Typography className={s.subtitle} variant={'body2'}>
+          Already have an account?
         </Typography>
-        <Button variant="link" as={Link} className={s.link} to={PATH.LOGIN}>
-          {t('Sign In')}
+        <Button as={Link} className={s.link} to={ROUTES.signIn} variant={'link'}>
+          Sign In
         </Button>
       </form>
     </Card>
