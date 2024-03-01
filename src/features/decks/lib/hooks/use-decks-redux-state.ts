5cb1ce23574a89b57bb26396a68c90fa271cd7fe
Alex666000 <bogdanov777000@mail.ru>
2024-03-01 19:04:52 +0300

    add slider searchParams

--- src/features/decks/lib/hooks/use-decks-redux-state.ts
+++ src/features/decks/lib/hooks/use-decks-redux-state.ts
@@ -1,15 +1,14 @@
 import { useCallback } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
 import { useMeQuery } from '@/features/auth/api/auth.api'
 import {
-  authorIdSelector,
   currentPageSelector,
   searchNameSelector,
   sliderValueSelector,
   tabValueSelector,
 } from '@/features/decks'
-import { sortOptionsSelector } from '@/features/decks/model/selectors/page-size-selector'
 import { Sort, useAppDispatch, useAppSelector } from '@/shared/lib'
 
 import { decksActions } from '../../model/slice/decks.slice'
@@ -18,72 +17,73 @@ import { decksActions } from '../../model/slice/decks.slice'
 export const useDecksReduxStateFilterParams = () => {
   const dispatch = useAppDispatch()
 
+  const [searchParams, setSearchParams]: [URLSearchParams, Function] = useSearchParams()
+  const params = Object.fromEntries(searchParams)
+
   const { data: meData } = useMeQuery()
 
   const searchName = useAppSelector(searchNameSelector) //
   const tabValue = useAppSelector(tabValueSelector) //
   const sliderValue = useAppSelector(sliderValueSelector) //
-  const authorId = useAppSelector(authorIdSelector)
-  const sortOptions = useAppSelector(sortOptionsSelector)
   const currentPage = useAppSelector(currentPageSelector)
 
-  // устанавливаю сортировку выбранную юзером
-  const onSetSortChange = (orderBy: Sort) => {
-    // orderBy - с сервера получим
-    dispatch(decksActions.setSortedString({ sortOptions: orderBy }))
-  }
+  const onSetCurrentPageChange = useCallback(() => {
+    return (newPage: number) => {
+      setSearchParams({ ...params, newPage })
+      dispatch(decksActions.setCurrentPage({ newPage }))
+    }
+  }, [dispatch, params, setSearchParams])
 
-  const onClearFilterParamsChange = () => {
-    dispatch(decksActions.clearFilterParams())
-    dispatch(decksActions.setSliderValue({ newSliderValue: [] }))
-    toast.info('Filters reset', { containerId: 'common' })
-  }
-
-  const onSetCurrentPageChange = (newPage: number) => {
-    dispatch(decksActions.setCurrentPage({ newPage }))
-  }
-
-  const onSetPageSizeChange = (newPageSize: string) => {
-    dispatch(decksActions.setPageSize({ newPageSize: Number(newPageSize) }))
-  }
+  const onSetPageSizeChange = useCallback(() => {
+    return (newPageSize: string) => {
+      setSearchParams({ ...params, newPageSize })
+      dispatch(decksActions.setPageSize({ newPageSize: Number(newPageSize) }))
+    }
+  }, [dispatch, params, setSearchParams])
 
   // устанавливаем в редюсер новый ТЕКСТ что ввел пользователь в инпут
   const handleSetSearchNameChange = useCallback(
     (newSearchName: string) => {
+      setSearchParams({ ...params, newSearchName })
       dispatch(decksActions.setSearchName({ newSearchName }))
     },
-    [dispatch]
+    [dispatch, params, setSearchParams]
   )
 
   // устанавливаем выбранный Таб юзером
   const handleSetTabValueChange = useCallback(
     (newUserTabValue: string) => {
       dispatch(decksActions.resetOnDefaultCurrentPage())
+      setSearchParams({ ...params, newUserTabValue })
       dispatch(decksActions.setTabsValue({ newUserTabValue }))
       if (meData) {
+        setSearchParams({ ...params, newUserTabValue: meData.id })
         dispatch(decksActions.setAuthorId({ newUserTabValue: meData.id }))
       }
       if (tabValue === 'my') {
+        setSearchParams({ ...params, newUserTabValue: undefined })
         dispatch(decksActions.setAuthorId({ newUserTabValue: undefined }))
       }
     },
-    [dispatch, meData, tabValue]
+    [dispatch, meData, params, setSearchParams, tabValue]
   )
 
   // сеттаем выбранное значение СЛАЙДЕРА
   const handleSetSliderValueChange = useCallback(
     (newSliderValue: number[]) => {
-      // сьрасываем страницы на 1
+      // сбрасываем страницы на 1
       dispatch(decksActions.resetOnDefaultCurrentPage())
       // ставим новые значения слайдера
+      setSearchParams({ ...params, newSliderValue })
       dispatch(decksActions.setSliderValue({ newSliderValue }))
+      setSearchParams({ ...params, max: newSliderValue[1], min: newSliderValue[0] })
       dispatch(
         decksActions.setCardsCount({
           cardsCount: { max: newSliderValue[1], min: newSliderValue[0] },
         })
       )
     },
-    [dispatch]
+    [dispatch, params, setSearchParams]
   )
 
   // возвращаем значения чтобы пользоваться на UI
